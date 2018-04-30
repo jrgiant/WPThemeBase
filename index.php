464ebb5715f42cfae0cfa5ad816e89762c288c2e
Joshua Rose <jrgiant_git@outlook.com>
2018-04-30 15:41:15 -0400

    Initial set up

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+
+<div class="main-content">Hello I am the main content</div>
+<div class="sidebar"><?php get_sidebar()?></div>
+<?php get_footer();
+//TODO: get footer
+
+?>
