223e95b42d36b6a18cf8829d166b3b64ac34f39e
Joshua Rose <jrgiant_git@outlook.com>
2018-05-11 14:46:41 -0400

    Adding cusomizer control support
    Hide and show for top bar tools

---
+++ customizer/customizer-controls.js
@@ -0,0 +1,25 @@
+(function(customize){
+    console.log('customize.bind.ready');
+    customize.bind('ready', ()=>{
+        console.log('customize.bind.ready');
+        customize('show_top_bar', setting=>{
+            console.log('customize.showtopbar called')
+            //originally from customize-controls.js from twentyseventeen
+            var visibility = function (control) {
+                console.log('visibitlity called')
+                if ('show' === setting.get()) {
+                    control.container.slideDown(180);
+                } else {
+                    control.container.slideUp(180);
+                }
+            };
+            var showSubItems = ()=>{
+            customize.control('top_bar_left', control=>{
+                visibility(control);
+            });
+            }//./showsubitems
+           showSubItems();
+           setting.bind(showSubItems); 
+        });
+    })//./customize.bind(ready)
+}(wp.customize));
