0e759582770662b0e6550449f04d49e98e7bbe88
Joshua Rose <jrgiant_git@outlook.com>
2018-05-11 16:53:42 -0400

    adding video to header

--- header.php
+++ header.php
@@ -18,13 +18,18 @@
     </button>
 </div>
 <div class="site-title">
+  <div class="filter"></div>
+        <video autoplay loop>
+            <source src="<?php echo trailingslashit(get_template_directory_uri()); ?>assets/media/Typing.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+            <source src="<?php echo trailingslashit(get_template_directory_uri()); ?>assets/media/Typing.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+        </video>
   <h1><?php bloginfo();?></h1>
 </div>
 <div class="navigation">
     <?php if (get_theme_mod('show_top_bar', 'show') === "show"): ?>
   <div class="top-bar">
-    <div class="left"><?php echo get_theme_mod('top_bar_left', "Enter information"); ?></div>
-    <div class="right"><?php echo get_theme_mod('top_bar_right', "Add a call to Action!"); ?></div>
+    <div class="left"><?php echo get_theme_mod('top_bar_left', "<a href=\"#\">Enter information</a>"); ?></div>
+    <div class="right"><?php echo get_theme_mod('top_bar_right', '<div class="button">Add a call to Action!</div>'); ?></div>
   </div>
   <?php endif;?>
   <!--./top-bar-->
