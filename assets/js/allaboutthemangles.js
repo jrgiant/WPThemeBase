cb8020a9b7ed1b6da5f02620f3b6b228fcc48e13
Joshua Rose <jrgiant_git@outlook.com>
2018-06-05 16:50:30 -0400

    adding Hamburgers.scss

--- assets/js/allaboutthemangles.js
+++ assets/js/allaboutthemangles.js
@@ -1,4 +1,14 @@
 ;(function(){
     let isDev = true;//Enable development mode in javascript.
     window.aata = window.aata || {isDev:isDev};
+    const menuHamburger = document.querySelector("button.hamburger");
+    const navMenu = document.querySelector(".navigation");
+    const animateHamburger = h => {
+        let movement = "100vw";
+        h.target.closest("button").classList.toggle("is-active");
+        if (h.target.closest("button").classList.contains("is-active"))
+            movement = "0";
+        navMenu.style.transform = `translateX(${movement})`;
+    };
+    menuHamburger.addEventListener("click", animateHamburger);
 }());
